75afe55d7816a7571578b9d9217a72774bd850d6
Gaurav Shah <shahg152002@gmail.com>
2024-12-02 08:47:44 +0530

    variable name update

--- 2024/Day_1_01.cpp
+++ 2024/Day_1_01.cpp
@@ -4,29 +4,29 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     ifstream file("input_01.txt");
-    
+
     if (!file.is_open()) {
         cout << "problem with opening file." << endl;
         return -1;
     }
     else cout << "filed open successfully" << endl;
 
-    vector<long> arr1;
-    vector<long> arr2;
+    vector<long> list1;
+    vector<long> list2;
     int a,b;
     while(file >> a >> b) {
-        arr1.push_back(a);
-        arr2.push_back(b);
+        list1.push_back(a);
+        list2.push_back(b);
     }
     
-    if(arr1.empty()) cout << "arr1 empty";
+    if(list1.empty()) cout << "arr1 empty";
 
-    sort(arr1.begin(), arr1.end());
-    sort(arr2.begin(), arr2.end());
+    sort(list1.begin(), list1.end());
+    sort(list2.begin(), list2.end());
 
     long ans=0;
-    for(long i=0;i<arr1.size();i++){
-        ans += abs(arr1[i]-arr2[i]);
+    for(long i=0;i<list1.size();i++){
+        ans += abs(list1[i]-list2[i]);
     }
 
     cout << ans;
