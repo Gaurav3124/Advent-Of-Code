56df175b0095eddd827cc4ffeedfa0fcb4ba15e2
Gaurav Shah <shahg152002@gmail.com>
2024-12-02 09:12:54 +0530

    test commit

--- 2024/Day_1_02.cpp
+++ 2024/Day_1_02.cpp
@@ -7,23 +7,23 @@ int main() {
     if (!file.is_open()) {cout << "problem with opening file." << endl; return -1;}
     else cout << "filed open successfully" << endl;
 
-    vector<long> arr1;
-    vector<long> arr2;
+    vector<long> list1;
+    vector<long> list2;
     unordered_map<int,int> mp2;
     int a,b;
     while(file >> a >> b) {
-        arr1.push_back(a);
-        arr2.push_back(b);
+        list1.push_back(a);
+        list2.push_back(b);
         mp2[b]++;
     }
     
-    if(arr1.empty()) cout << "arr1 empty";
+    if(list1.empty()) cout << "list1 empty";
     
 
     long ans=0;
-    for(long i=0;i<arr1.size();i++){
-        if(mp2.find(arr1[i])!= mp2.end()){
-            ans += mp2[arr1[i]] * arr1[i];
+    for(long i=0;i<list1.size();i++){
+        if(mp2.find(list1[i])!= mp2.end()){
+            ans += mp2[list1[i]] * list1[i];
         }
     }
 
